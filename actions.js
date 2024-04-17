990ce6e559d1ef6a1637f2c06997289cf7d2c39a
tiko otarashvili <101200636+tikoot@users.noreply.github.com>
2024-04-17 20:01:54 +0400

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,18 @@
+import { AUTH_COOKIE_KEY } from "@/constants";
+import { cookies } from "next/headers";
+
+export async function login(username, password) {
+  const response = await fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+
+  const user = response.json();
+  const cookieStore = cookies();
+  cookieStore.set(AUTH_COOKIE_KEY, JSON.stringify(user));
+  console.log(response);
+}
